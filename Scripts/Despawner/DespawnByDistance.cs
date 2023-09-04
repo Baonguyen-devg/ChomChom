8ccf8d7e689a694eace88788d5f3f52ee3a88199
Nguyễn Thái Bảo <44467739+Baonguyen3303@users.noreply.github.com>
2023-09-04 10:48:23 +0700

    Add despawn by distance

---
+++ Scripts/Despawner/DespawnByDistance.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnByDistance : Despawner
+{
+    private const float DEFAULT_DISTANCE_DESPAWN = 11f;
+    [SerializeField] private float distanceDespawn = DEFAULT_DISTANCE_DESPAWN;
+
+    [SerializeField] private float distance;
+
+    /*Begin predicatedload of components*/
+    [SerializeField] private List<System.Action> loadComponentActions;
+
+    [SerializeField] private Transform pointEndLand;
+    [SerializeField] private Transform pointDespawnLand;
+    /*End predicatedload of components*/
+
+    protected override void LoadComponent()
+    {
+        loadComponentActions = new List<System.Action>
+        {
+            () => this.pointEndLand = transform.parent.Find("End_Point_Land"),
+            () => this.pointDespawnLand = GameObject.Find("Main_Camera").transform.Find("Point_Despawn_Land")
+        };
+
+        foreach (var action in loadComponentActions)
+            action?.Invoke();
+    }
+
+    public override void DespawnObject() =>
+        LandSpawner.Instance.Despawn(transform.parent);
+
+    protected override bool CanDespawn()
+    {
+        this.distance = Vector3.Distance(this.pointEndLand.position, this.pointDespawnLand.position);
+        if (this.distance <= this.distanceDespawn) return true;
+        else return false;
+    }
+}
